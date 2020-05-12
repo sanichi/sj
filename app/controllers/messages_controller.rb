48e5c6d7a8fb679aeacdc0edaf488bfb77935054
Mark Orr <mark.j.l.orr@googlemail.com>
2020-05-12 16:37:27 +0100

    What to do if game is deleted

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,32 @@
 class MessagesController < ApplicationController
   authorize_resource class: false
+  before_action :get_player
 
   def pull
-    player = Player.find(params[:player_id])
-    game = player.game
-    last_message_id = params[:last_message_id].to_i
+    if @player
+      last_message_id = params[:last_message_id].to_i
 
-    @messages = game.messages.where("id > ?", last_message_id)
-    @last_message_id = last_message_id
-    @messages.each do |m|
-      @last_message_id = m.id if m.id > @last_message_id
+      @messages = @game.messages.where("id > ?", last_message_id)
+      @last_message_id = last_message_id
+      @messages.each do |m|
+        @last_message_id = m.id if m.id > @last_message_id
+      end
     end
   end
 
   def push
-    player = Player.find(params[:player_id])
-    game = player.game
-    if params[:card_index]
-      game.messages.create(player_id: player.id, reveal: params[:card_index].to_i)
+    if @player
+      if params[:card_index]
+        @game.messages.create(player_id: @player.id, reveal: params[:card_index].to_i)
+      end
     end
+  end
+
+  private
+
+  def get_player
+    @player = Player.find_by(id: params[:player_id])
+    @game = @player.game if @player
+    render "abort" unless @player
   end
 end
