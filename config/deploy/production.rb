e4289292bd8d4464853497b32b86e7889f9c90e6
Mark Orr <mark.j.l.orr@googlemail.com>
2022-02-28 17:09:06 +0000

    Delpoy to nihon

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "thessaloniki.sanichi.me", user: "sanichi", roles: %w{web app db}
+server "nihon.sanichi.me", user: "sanichi", roles: %w{web app db}
 set :deploy_to, "/var/www/me.sj"
 set :rails_env, "production"
 
