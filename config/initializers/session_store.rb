efa7037a17971ce7d969a8362f1b0727a5ab19e2
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-10 19:25:14 +0000

    Gem update and security overhaul

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 Rails.application.config.session_store :cookie_store,
-  key: "_sj_app_session",
+  key: "#{Rails.env.production? ? '__Secure-' : ''}Sj-Session",
   expire_after: 8.weeks
