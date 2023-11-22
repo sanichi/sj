8890954fc108c0cac0210e3a968d65700db90048
Mark Orr <mark.j.l.orr@googlemail.com>
2023-11-22 19:48:11 +0000

    Update to rails 7.1.2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   # see https://github.com/CanCanCommunity/cancancan/wiki/Non-RESTful-Controllers
   authorize_resource class: false
+
+  def env
+    dirs = `ls /home/sanichi/.passenger/native_support`
+    vers = dirs.scan(/\d*\.\d*\.\d*/)
+    @passenger_version = vers.any? ? vers.last : "not found"
+    vers = ActiveRecord::Base.connection.execute('select version();').values[0][0] rescue "oops"
+    @postgres_version = vers.match(/(1[4-6]\.\d+)/)? $1 : "not found"
+    @host = ENV["HOSTNAME"] || `hostname`.chop.sub(".local", "")
+  end
 end
