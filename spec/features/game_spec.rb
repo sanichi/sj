ff8c1317e3af854145000bb55a14d14aafacf5b4
Mark Orr <mark.j.l.orr@googlemail.com>
2025-02-01 21:56:18 +0000

    The last change fixed the problem with features/game_spec

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -32,13 +32,12 @@ describe Game, js: true do
       expect(g.four).to eq data.four
     end
 
-    # FIX: something is wrong with JS here
-    it "delete game", js: false do
+    it "delete game" do
       expect(Game.count).to eq 1
 
-      # accept_confirm do
+      accept_confirm do
         click_link t("game.delete")
-      # end
+      end
 
       expect(page).to have_title t("game.games")
       expect(Game.count).to eq 0
