a2eeb0942bb135e2a93c81171dfa07bcc38b59b7
Mark Orr <mark.j.l.orr@googlemail.com>
2025-10-30 16:11:10 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_163338) do
+ActiveRecord::Schema[8.1].define(version: 2022_03_30_163338) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.bigint "user_id"
+    t.datetime "created_at", null: false
     t.boolean "debug", default: false
-    t.integer "participants", limit: 2, default: 2
-    t.boolean "shuffle", default: true
-    t.string "state", limit: 10, default: "waiting"
-    t.integer "upto", limit: 2, default: 100
-    t.integer "votes", limit: 2, default: 0
-    t.integer "m2", limit: 2, default: 5
+    t.boolean "four", default: false
+    t.integer "hand", limit: 2, default: 1
     t.integer "m1", limit: 2, default: 10
+    t.integer "m2", limit: 2, default: 5
     t.integer "p0", limit: 2, default: 15
     t.integer "p1", limit: 2, default: 10
+    t.integer "p10", limit: 2, default: 10
+    t.integer "p11", limit: 2, default: 10
+    t.integer "p12", limit: 2, default: 10
     t.integer "p2", limit: 2, default: 10
     t.integer "p3", limit: 2, default: 10
     t.integer "p4", limit: 2, default: 10
@@ -34,47 +34,46 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_30_163338) do
     t.integer "p7", limit: 2, default: 10
     t.integer "p8", limit: 2, default: 10
     t.integer "p9", limit: 2, default: 10
-    t.integer "p10", limit: 2, default: 10
-    t.integer "p11", limit: 2, default: 10
-    t.integer "p12", limit: 2, default: 10
-    t.datetime "created_at", null: false
+    t.integer "participants", limit: 2, default: 2
+    t.boolean "shuffle", default: true
+    t.string "state", limit: 10, default: "waiting"
     t.datetime "updated_at", null: false
-    t.integer "hand", limit: 2, default: 1
-    t.boolean "four", default: false
+    t.integer "upto", limit: 2, default: 100
+    t.bigint "user_id"
+    t.integer "votes", limit: 2, default: 0
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.bigint "game_id"
     t.string "json"
-    t.bigint "target", default: 0
     t.boolean "only_start", default: false
-    t.datetime "created_at", null: false
+    t.bigint "target", default: 0
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_messages_on_game_id"
   end
 
   create_table "players", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "game_id"
-    t.integer "score", limit: 2, default: 0
-    t.integer "pscore", limit: 2, default: 0
-    t.integer "place", limit: 2, default: 0
-    t.boolean "dealt", default: false
     t.datetime "created_at", null: false
+    t.boolean "dealt", default: false
+    t.bigint "game_id"
+    t.integer "place", limit: 2, default: 0
+    t.integer "pscore", limit: 2, default: 0
+    t.integer "score", limit: 2, default: 0
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["game_id"], name: "index_players_on_game_id"
     t.index ["user_id"], name: "index_players_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", limit: 15
-    t.string "last_name", limit: 15
-    t.string "handle", limit: 10
-    t.string "password_digest"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
+    t.string "first_name", limit: 15
+    t.string "handle", limit: 10
+    t.string "last_name", limit: 15
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
-
 end
